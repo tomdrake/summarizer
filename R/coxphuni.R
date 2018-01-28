0e9bf50e9ea93e55c71aec4cd823238d1c5a47d4
tomdrake <tdrake@doctors.org.uk>
2018-01-28 11:06:33 +0000

    Update coxphuni.R

--- R/coxphuni.R
+++ R/coxphuni.R
@@ -1,10 +1,16 @@
 # Survival
-coxphuni <- function(df.in, dependent, explanatory){
-	require(survival)
-	result <- list()
-	for (i in 1:length(explanatory)){
-		result[[i]] <- coxph(as.formula(paste0(dependent, "~", explanatory[i])), data=df.in)
-	}
-	class(result) = "coxphlist"
-	return(result)
+coxphuni <- function(df.in, dependent, explanatory, weights = NULL){
+  require(survival)
+  result <- list()
+  if (is.null(weights)){
+    for (i in 1:length(explanatory)){
+      result[[i]] <- coxph(as.formula(paste0(dependent, "~", explanatory[i])), data=df.in)
+    } 
+  }else{
+    for (i in 1:length(explanatory)){
+      result[[i]] <- coxph(as.formula(paste0(dependent, "~", explanatory[i])), weights = weights, data=df.in)
+    }
+  }
+  class(result) = "coxphlist"
+  return(result)
 }
