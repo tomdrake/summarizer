603cb486f74327bcdbf65e0acc30035f94c974c1
tomdrake <tdrake@doctors.org.uk>
2018-01-28 17:22:46 +0000

    Update glmmulti.R

--- R/glmmulti.R
+++ R/glmmulti.R
@@ -5,14 +5,14 @@ glmmulti <- function(df.in, dependent, explanatory, weights = NULL, family = "bi
   result = list()
   if (is.null(weights)){
     for (i in 1:length(dependent)) {          
-      f <- as.formula(paste(dependent, '~', paste(explanatory, collapse="+")))
+      f <- as.formula(paste(dependent[i], '~', paste(explanatory, collapse="+")))
       fit <- do.call("glm", list(formula=f, data=df.in, 
                                  family= family))
       fit['call'] = deparse(f)
       result[[i]] <- fit}
   } else {
     for (i in 1:length(dependent)) {          
-      f <- as.formula(paste(dependent, '~', paste(explanatory, collapse="+")))
+      f <- as.formula(paste(dependent[i], '~', paste(explanatory, collapse="+")))
       fit <- do.call("glm", list(formula=f, data=df.in, 
                                  family= family, weights = weights))
       fit['call'] = deparse(f)
