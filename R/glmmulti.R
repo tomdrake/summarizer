7cd3e9fb41282190903f25d741be01ca20374988
tomdrake <tdrake@doctors.org.uk>
2018-01-28 11:03:41 +0000

    Update glmmulti.R

--- R/glmmulti.R
+++ R/glmmulti.R
@@ -1,12 +1,25 @@
 # Function to run standard multivariable logistic regression ----
 # Note, takes multiple explanatory AND dependent variables
 # Dependent variables are run indiviudally, and models returned as a names list (setNames)
-glmmulti <- function(df.in, dependent, explanatory){
-	result = list()
-	for (i in 1:length(dependent)){
-		result[[i]] = glm(paste(dependent[i], "~", paste(explanatory, collapse="+")), data=df.in, family="binomial")
-	}
-	result = setNames(result, dependent)
-	class(result) = "glmlist"
-	return(result)
+glmmulti <- function(df.in, dependent, explanatory, weights = NULL, family = "binomial"){
+  result = list()
+  if (is.null(weights)){
+    for (i in 1:length(dependent)) {          
+      f <- as.formula(paste(dependent, '~', paste(explanatory, collapse="+")))
+      fit <- do.call("glm", list(formula=f, data=df.in, 
+                                 family= family))
+      fit['call'] = deparse(f)
+      result[[i]] <- fit}
+  } else {
+    for (i in 1:length(dependent)) {          
+      f <- as.formula(paste(dependent, '~', paste(explanatory, collapse="+")))
+      fit <- do.call("glm", list(formula=f, data=df.in, 
+                                 family= family, weights = weights))
+      fit['call'] = deparse(f)
+      result[[i]] <- fit}
+  }
+  result = setNames(result, dependent)
+  class(result) = "glmlist"
+  return(result)
 }
+
