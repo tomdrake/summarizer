518a49c636839c3c4696022dc06d42a3e043c372
tomdrake <tdrake@doctors.org.uk>
2018-01-28 11:02:58 +0000

    Update coxphmulti.R

--- R/coxphmulti.R
+++ R/coxphmulti.R
@@ -1,11 +1,18 @@
-# Survival
-coxphmulti <- function(df.in, dependent, explanatory){
-	require(survival)
-	result = list()
-	for (i in 1:length(dependent)){
-		result[[i]] = coxph(as.formula(paste0(dependent, "~", paste(explanatory, collapse="+"))), data=df.in)
-	}
-	result = setNames(result, dependent)
-	class(result) = "coxphlist"
-	return(result)
+#Survival multivariable with weights - clusters also possible
+coxphmulti <- function(df.in, dependent, explanatory, weights = NULL){
+  require(survival)
+  result = list()
+  if (is.null(weights)){
+  for (i in 1:length(dependent)){
+    result[[i]] = coxph(as.formula(paste0(dependent, "~", paste(explanatory, collapse="+"))), data=df.in)
+  }
+  }else{
+    for (i in 1:length(dependent)){
+      result[[i]] = coxph(as.formula(paste0(dependent, "~", paste(explanatory, collapse="+"))), weights = weights, data=df.in)
+    }
+  }
+  result = setNames(result, dependent)
+  class(result) = "coxphlist"
+  return(result)
 }
+
