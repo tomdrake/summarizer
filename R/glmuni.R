ee714dfe55056eeaccd286346dff6cdcd3a1963e
tomdrake <tdrake@doctors.org.uk>
2018-01-28 17:27:46 +0000

    Update glmuni.R

--- R/glmuni.R
+++ R/glmuni.R
@@ -1,15 +1,22 @@
-# Function multiple univariable logistic regressions against a list of explanatory variables -----------
-glmuni <- function(df.in, dependent, explanatory, weights = NULL){
-  result <- list()
+#includes weights
+glmuni <- function(df.in, dependent, explanatory, weights = NULL, family = "binomial"){
+  result = list()
   if (is.null(weights)){
-  for (i in 1:length(explanatory)){
-    result[[i]] <- glm(paste(dependent, "~", explanatory[i]), data=df.in, family="binomial")
-    }
-    } else {
-      for (i in 1:length(explanatory)){
-      result[[i]] <- glm(as.formula(paste0(dependent, "~", explanatory[i])), weights = weights, data=df.in)
-    }
+    for (i in 1:length(explanatory)) {          
+      f <- as.formula(paste(dependent, '~', paste(explanatory[i], collapse="+")))
+      fit <- do.call("glm", list(formula=f, data=df.in, 
+                                 family= family))
+      fit['call'] = deparse(f)
+      result[[i]] <- fit}
+  } else {
+    for (i in 1:length(explanatory)) {          
+      f <- as.formula(paste(dependent, '~', paste(explanatory[i], collapse="+")))
+      fit <- do.call("glm", list(formula=f, data=df.in, 
+                                 family= family, weights = weights))
+      fit['call'] = deparse(f)
+      result[[i]] <- fit}
   }
+  result = setNames(result, dependent)
   class(result) = "glmlist"
   return(result)
 }
