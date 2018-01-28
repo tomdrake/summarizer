8bf27899cd379dc924109fcc9a49779d2b6692a4
tomdrake <tdrake@doctors.org.uk>
2018-01-28 11:12:42 +0000

    Update glmuni.R

--- R/glmuni.R
+++ R/glmuni.R
@@ -1,8 +1,14 @@
 # Function multiple univariable logistic regressions against a list of explanatory variables -----------
-glmuni <- function(df.in, dependent, explanatory){
+glmuni <- function(df.in, dependent, explanatory, weights = NULL){
   result <- list()
+  if (is.null(weights)){
   for (i in 1:length(explanatory)){
     result[[i]] <- glm(paste(dependent, "~", explanatory[i]), data=df.in, family="binomial")
+    }
+    } else {
+      for (i in 1:length(explanatory)){
+      result[[i]] <- glm(as.formula(paste0(dependent, "~", explanatory[i])), weights = weights, data=df.in)
+    }
   }
   class(result) = "glmlist"
   return(result)
